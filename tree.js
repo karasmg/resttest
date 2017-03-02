489aa1b22496a1e2c5d5f2cb0bfdb4220bd21c6e
Max <karasmg@gmail.com>
2017-03-02 17:10:21 +0200

    client side1+++++

--- tree.js
+++ tree.js
@@ -15,12 +15,77 @@ yii2AngApp_tree.config(['$routeProvider', function($routeProvider) {
 yii2AngApp_tree.controller('treeindex', ['$scope', '$http',
     function($scope,$http) {
         $scope.message = 'Это страница с деревом элементов';
-        $scope.add_node = function(){
-            console.log('add_node');
+        $scope.items = [
+            { id: 1, name: 'First', childrens: [
+                { id: 3, name: 'three', childrens: null },
+                { id: 4, name: 'four', childrens: [
+                    { id: 5, name: 'five', childrens: null }
+                ]
+                }
+            ]
+            },
+            { id: 2, name: 'second', childrens: [] }
+        ];
+        $scope.say = function (str) {
+            alert(str);
         }
+        $scope.meee = function(nodes){
+            $scope.nodes = nodes;
+        }
+
+
+        $scope.addNode = function(){
+
+        }
+
+        $scope.delNode = function(){
+
+        }
+
+
+        $scope.nodes = [
+            {
+                id: 0,
+                text: "Родительский узел",
+                nodes: [
+                    {
+                        id: 1,
+                        text: "узел1",
+                        nodes: [
+                            {
+                                id: 2,
+                                text: "узел2"
+                            }
+                        ]
+                    },
+                    {
+                        id: 3,
+                        text: "узел3",
+                        nodes: [
+                            {
+                                id: 4,
+                                text: "узел4"
+                            },
+                            {
+                                id: 5,
+                                text: "узел5",
+                                nodes: [
+                                    {
+                                        id: 6,
+                                        text: "узел6"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
+
     }]);
 
 yii2AngApp_tree.directive('ngNode', ['$document', '$compile', function($document, $compile) {
+
     return function(scope, element, attrs) {
         element.on('contextmenu', function(event) {
             event.preventDefault();
@@ -60,7 +125,8 @@ yii2AngApp_tree.directive('ngNode', ['$document', '$compile', function($document
             }
             $document.on('click', menu_click);
             scope.add_node = function(){
-                console.log('add_node');
+                //scope.meee(nooo);
+                console.log('add_node', element);
                 menu_remove();
             }
             scope.del_node = function(){
@@ -75,6 +141,62 @@ yii2AngApp_tree.directive('ngNode', ['$document', '$compile', function($document
     };
 }]);
 
+//объявляет дерево
+yii2AngApp_tree.directive('myTreeview', [function () {
+        return {
+            restrict: 'A',
+            priority: 1001,
+            controller: 'treeindex',
+            compile: function (element, tAttrs) {
+                //создаем шаблон для дочерних элементов
+                angular.forEach(angular.element(element.find('li')), function (item) {
+                    var el = angular.element(item);
+
+                    el.prepend(angular.element('<i />').addClass('normal').attr('ng-hide', 'config.hasChildrens'));
+                    el.prepend(angular.element('<i />').addClass('expanded').attr('ng-show', 'config.hasChildrens && !config.collapsed').attr('ng-click', 'collapse(config)'));
+                    el.prepend(angular.element('<i />').addClass('collapsed').attr('ng-show', 'config.hasChildrens && config.collapsed').attr('ng-click', 'collapse(config)'));
+                });
+
+                var itemTemplate = element.html();
+                var template = angular.element('<ul ng-hide="config.collapsed" />').append(itemTemplate)[0].outerHTML;
+
+                return function (scope, element, attrs, ctrl) {
+                    //делаем доступным шаблон для дочерней дерективы
+                    ctrl.Template = template;
+                    element.addClass('treeview');
+                    scope.collapse = function (config) {
+                        config.collapsed = !config.collapsed;
+                    }
+
+                }
+            }
+        }
+    } ])
+
+    //достраивает дочерние элемент
+    .directive('myTreeviewChilds', ['$compile', '$parse', function ($compile, $parse) {
+        return {
+            restrict: 'A',
+            require: '^myTreeview',
+            link: function (scope, element, attrs, ctrl) {
+                //достаем дочерние элементы
+                var items = $parse(attrs.myTreeviewChilds)(scope);
+                var newScope = scope.$new();
+                newScope.items = items
+                scope.config = {};
+                if (items != null && items.length > 0) {
+                    newScope.$parent.config.hasChildrens = true;
+                    newScope.$parent.config.collapsed = false;
+                    element.append($compile(ctrl.Template)(newScope));
+                }
+                else {
+                    newScope.$parent.config.hasChildrens = false;
+                    newScope.$parent.config.collapsed = false;
+                }
+            }
+        }
+    } ]);
+
 yii2AngApp_tree.directive('ngRightClick', function($parse) {
     return function(scope, element, attrs) {
         var fn = $parse(attrs.ngRightClick);
@@ -86,8 +208,6 @@ yii2AngApp_tree.directive('ngRightClick', function($parse) {
         });
     };
 });
-
-
 
 function closest(e, className) {
     if (e.nodeName == "HTML") {
