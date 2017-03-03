ff90a402d1f7f2e18e9efaf8509e31b4d4698669
Max <karasmg@gmail.com>
2017-03-03 11:47:02 +0200

    работает добавление и удаление узлов

--- tree.js
+++ tree.js
@@ -15,35 +15,8 @@ yii2AngApp_tree.config(['$routeProvider', function($routeProvider) {
 yii2AngApp_tree.controller('treeindex', ['$scope', '$http',
     function($scope,$http) {
         $scope.message = 'Это страница с деревом элементов';
-        $scope.items = [
-            { id: 1, name: 'First', childrens: [
-                { id: 3, name: 'three', childrens: null },
-                { id: 4, name: 'four', childrens: [
-                    { id: 5, name: 'five', childrens: null }
-                ]
-                }
-            ]
-            },
-            { id: 2, name: 'second', childrens: [] }
-        ];
-        $scope.say = function (str) {
-            alert(str);
-        }
-        $scope.meee = function(nodes){
-            $scope.nodes = nodes;
-        }
 
-
-        $scope.addNode = function(){
-
-        }
-
-        $scope.delNode = function(){
-
-        }
-
-
-        $scope.nodes = [
+        $scope.nodes1 = [
             {
                 n_id: 0,
                 n_parent: 0,
@@ -63,28 +36,34 @@ yii2AngApp_tree.controller('treeindex', ['$scope', '$http',
                 nodes: [
                     {
                         id: 1,
+                        parent: 0,
                         text: "узел1",
                         nodes: [
                             {
                                 id: 2,
+                                parent: 1,
                                 text: "узел2"
                             }
                         ]
                     },
                     {
                         id: 3,
+                        parent: 0,
                         text: "узел3",
                         nodes: [
                             {
                                 id: 4,
+                                parent: 3,
                                 text: "узел4"
                             },
                             {
                                 id: 5,
+                                parent: 3,
                                 text: "узел5",
                                 nodes: [
                                     {
                                         id: 6,
+                                        parent: 5,
                                         text: "узел6"
                                     }
                                 ]
@@ -95,7 +74,65 @@ yii2AngApp_tree.controller('treeindex', ['$scope', '$http',
             }
         ];
 
+        $scope.addNode = function(id){
+            var node = $scope.findNode(id);
+            if(node.nodes === undefined)
+                node.nodes = [];
+            node.nodes.push({
+                id: $scope.getLastId(),
+                parent: id,
+                text: 'Узел',
+                nodes: []
+            });
+        }
+
+        $scope.delNode = function(parent, id){
+            var node = $scope.findNode(parent);
+            for(var i=0; i < node.nodes.length; i++){
+                if(node.nodes[i].id == id){
+                    node.nodes.splice(i,1);
+                }
+            }
+        }
+
+
+        $scope.getLastId = function(){
+            var last=0;
+            recurs($scope.nodes);
+            last++;
+            return last;
+            function recurs(nodes){
+                var l = nodes.length;
+                for(var i=0; i<l; i++ ){
+                    if(nodes[i].id > last)
+                        last =  nodes[i].id;
+                    if(nodes[i].nodes !== undefined)
+                        recurs(nodes[i].nodes);
+                }
+            }
+        }
+
+
+        $scope.findNode = function(id){
+            var rez={};
+            recurs($scope.nodes);
+            return rez;
+            function recurs(nodes){
+                var l = nodes.length;
+                for(var i=0; i<l; i++ ){
+                    if(nodes[i].id == id) {
+                        rez =  nodes[i];
+                        return;
+                    }
+                    if(nodes[i].nodes !== undefined)
+                        recurs(nodes[i].nodes);
+                }
+            }
+        }
     }]);
+
+
+
 
 yii2AngApp_tree.directive('ngNode', ['$document', '$compile', function($document, $compile) {
 
@@ -120,34 +157,41 @@ yii2AngApp_tree.directive('ngNode', ['$document', '$compile', function($document
                                 '</ul>';
             context_menu = angular.element(context_menu);
             context_menu.css({
-                top: (coord.top+coord.height)+"px",
+                top: (coord.top+15)+"px",
                 left: (coord.left+coord.width/2)+"px",
                 display: "block"
             });
             element.append(context_menu);
             $compile(context_menu)(scope);
+
+            //Удаление меню с экрана
             var menu_remove = function(){
                 context_menu.remove();
                 event.stopPropagation();
                 $document.off('click', menu_click);
             };
+
+            //Клик вне меню
             var menu_click = function(event){
-                console.log('body_click');
                 if (closest( event.target, "dropdown-menu").nodeName !== undefined) return;
                 menu_remove();
             }
             $document.on('click', menu_click);
+
+            //Клик по пункту Добавить
             scope.add_node = function(){
-                //scope.meee(nooo);
-                console.log('add_node', element);
+                scope.addNode(attrs.id);
                 menu_remove();
             }
+
+            //Клик по пункту Удалить
             scope.del_node = function(){
-                console.log('del_node');
+                scope.delNode(attrs.parent, attrs.id);
                 menu_remove();
             }
+
+            //Клик по пункту Переместить
             scope.move_node = function(){
-                console.log('move_node');
                 menu_remove();
             }
         });
